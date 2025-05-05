ddb38d64e6c9b3ee2cfaa3d0c7b54d33f376a3fe
curlycrow <call.mecrow9@gmail.com>
2025-05-05 10:26:11 +0700

    update main page

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -37,7 +37,7 @@ describe('Login Auth', () => {
         
     })
 
-    it.only('failed login with using wrong username', () => {
+    it('failed login with using wrong username', () => {
         cy.get("#mat-input-0").type("miss_rindu")
         cy.get("#mat-input-1").type("Good_4you", { sensitive: true })
         cy.intercept('POST','/api/login').as('invalidateUsername')
--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -4,7 +4,7 @@ describe('Functional on dashboard main page', () => {
         
     })
   
-    it.only('Verify Homepage Loads Successfully', () => {
+    it('Verify Homepage Loads Successfully', () => {
       cy.get("app-price-filter").should("contain","Price Filter")
       cy.get("mat-list-item").should("have.length", 6)
       cy.get("mat-list-item").eq(0).should("contain", "All Categories")
@@ -15,15 +15,33 @@ describe('Functional on dashboard main page', () => {
       
     })
   
-    it.skip('Verify Header Section Elements', () => {
+    it('Verify Header Section Elements', () => {
+        cy.get("mat-toolbar-row div").should("have.length",3)
+        cy.get("mat-toolbar-row div span.mdc-button__label").contains("Book Cart")
+        cy.get("mat-toolbar app-search input").should("have.attr","placeholder", "Search books or authors")
+        cy.get("mat-toolbar mat-icon").should("have.length",2)
+        cy.get("mat-toolbar mat-icon").eq(1).contains("shopping_cart")
+        cy.get("mat-toolbar span.mdc-button__label").should("have.length",4)
+        cy.get("mat-toolbar span.mdc-button__label").eq(1).contains("Login")
       
     })
   
     it('Verify Book Listings Display Correctly', () => {
+        cy.get("mat-list-item").should("have.length", 6)
+        cy.get("mat-list-item").eq(0).should("contain", "All Categories")
+        cy.get("app-book-card").should("have.length",45)
+        cy.get("app-book-card mat-card").eq(1).contains("Harry Potter and the Prisoner of Azkaban")
+        cy.get("mat-list-item").eq(2).should("contain", "Fiction").click()
+        cy.get("app-book-card").should("have.length",10)
+        cy.get("app-book-card mat-card").eq(3).contains("A Princess in Theory: Reluctant Royals")
       
     })
   
-    it('Verify Search Functionality (Valid Keyword)', () => {
+    it.only('Verify Search Functionality (Valid Keyword)', () => {
+        cy.get("mat-toolbar app-search input").type("ministry")
+        cy.get("#mat-autocomplete-0").click()
+        cy.get("app-book-card mat-card").eq(0).contains("The Ministry of Truth: The Biography of George Orwell's \"1984\"")
+        //cy.get("mat-toolbar app-search input").contains("The Ministry of Truth: The Biography of George Orwell's \"1984\"")
       
     })
   
