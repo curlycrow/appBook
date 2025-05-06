5f54ac109cc435512d8d79591f6e4034bfab80a3
curlycrow <call.mecrow9@gmail.com>
2025-05-06 11:40:15 +0700

    cart function

--- cypress/e2e/purchase.cy.js
+++ cypress/e2e/purchase.cy.js
@@ -1,30 +1,51 @@
 import LoginPage from "../page-object/login-page"
 import basicUser from "../fixtures/basicUser.json"
 
-describe.only('purcashing book activity', () => {
+describe('purcashing book activity', () => {
     const loginPage= new LoginPage()
-    beforeEach('user must be login befor checkout book', () => {
+    beforeEach('user must be login before checkout book', () => {
         cy.visit("/login")
         loginPage.typeUsername(basicUser.username)
         loginPage.typePassword(basicUser.password)
         loginPage.clickLogin()
         cy.location("href").should("include", "/")
+        cy.get("mat-toolbar mat-icon").eq(1).contains("favorite")
         cy.get(".mdc-button__label").contains(basicUser.username).should("be.visible")
     })
 
-    it.only('Add a Single Book to Cart', () => {
+    
+
+    it('Add a Single Book to Cart', () => {
+        cy.get("app-book-card").should("have.length",45)
+        cy.get("app-book-card mat-card").eq(1).contains("Harry Potter and the Prisoner of Azkaban")
+        cy.get("app-book-card mat-card app-addtocart").eq(1).click()
+        cy.get("mat-toolbar mat-icon").eq(2).contains("shopping_cart").click()
+        cy.get("mat-card-content tbody td.mat-mdc-cell").eq(1).contains("Harry Potter and the Prisoner of Azkaban")
         
     })
 
-    it('Increase Book Quantity in Cart', () => {
-        
+    it.only('Increase Book Quantity from dashboard', () => {
+        cy.get("app-book-card mat-card").eq(1).contains("Harry Potter and the Prisoner of Azkaban")
+        cy.get("app-book-card mat-card app-addtocart").eq(1).click()
     })
 
-    it('Decrease Book Quantity in Cart', () => {
-        
+    it.only('Increase Book Quantity in Cart', () => {
+        cy.get("mat-toolbar mat-icon").eq(2).contains("shopping_cart").click()
+        cy.get("mat-card-content tbody td.mat-mdc-cell").eq(1).contains("Harry Potter and the Prisoner of Azkaban")
+        cy.get("mat-card-content tbody td button.mdc-icon-button").eq(1).contains("add_circle").click()
+        //cy.get("mat-card-content td.mat-icon").contains("delete").click()
     })
 
-    it('Remove a Book from Cart', () => {
+    it.only('Decrease Book Quantity in Cart', () => {
+        cy.get("mat-toolbar mat-icon").eq(2).contains("shopping_cart").click()
+        cy.get("mat-card-content tbody td.mat-mdc-cell").eq(1).contains("Harry Potter and the Prisoner of Azkaban")
+        cy.get("mat-card-content tbody td button.mdc-icon-button").eq(0).contains("remove_circle").click()
+    })
+
+    it.only('Remove a Book from Cart', () => {
+        cy.get("mat-toolbar mat-icon").eq(2).contains("shopping_cart").click()
+        cy.get("mat-card-content tbody td.mat-mdc-cell").eq(1).contains("Harry Potter and the Prisoner of Azkaban")
+        cy.get("mat-card-content tbody td button.mdc-icon-button").eq(2).contains("delete").click()
     
     })
 
